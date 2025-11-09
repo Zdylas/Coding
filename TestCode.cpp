b3cf48cd06a1eac8fc3526392aef934371e41401
Zdylas <dominic.mcnulty12@gmail.com>
2025-11-10 12:40:21 +1300

    Successful working first code and gitignore

--- TestCode.cpp
+++ TestCode.cpp
@@ -1,1 +1,11 @@
-# Test Code
+//Simple Program For Testing
+//
+//  Dominic McNulty
+//  10/11/2025
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!";
+  return 0;
+}
