fa05391a8f46ab9d4d672f8bdb2c510e64145a42
dmc274 <dmc274@uclive.ac.nz>
2026-07-17 16:06:16 +1200

    Learning more about fundamental types, added float information

--- Foundations/FundamentalTypes.cpp
+++ Foundations/FundamentalTypes.cpp
@@ -5,6 +5,7 @@
  */
 
 #include <iostream>
+#include <limits>
 
 
 int main()
@@ -23,14 +24,14 @@ int main()
 		// This is the typical implementation of char for x86 and x84 machines
 		{
 			signed char my_char = -128;
-			std::cout << "my_char: " << my_char << " and as an int: " << static_cast<int>(my_char) << std::endl;
+			std::cout << "my_char: " << my_char << " and as an int: " << static_cast<int>(my_char) << '\n';
 
 		}
 		// Unsigned char - ranges from 0 to 255
 		// This is the typical implementation of char for ARM and PowerPC
 		{
 			unsigned char my_char = 128;
-			std::cout << "my_char: " << my_char << " and as an int: " << static_cast<int>(my_char) << std::endl;
+			std::cout << "my_char: " << my_char << " and as an int: " << static_cast<int>(my_char) << '\n' << std::endl;
 		}
 	}
 
@@ -46,7 +47,7 @@ int main()
 
 			// out-of-range will fail silently for all integer types: overflows and wraps to -32768
 			short int another_int = 32768; 
-			std::cout << "Value of: " << another_int << std::endl; 
+			std::cout << "Value of: " << another_int << '\n' << std::endl; 
 		}
 
 		// int:
@@ -54,7 +55,7 @@ int main()
 		// Size of at least 16 bits. Most systems will be 32 bits including LLP64
 		{
 			int my_int = 2147483647; // min: -2147483648, max: 2147483647
-			std::cout << "Value of: " << my_int << ", Size: " << sizeof(my_int) << " bytes" << std::endl;
+			std::cout << "Value of: " << my_int << ", Size: " << sizeof(my_int) << " bytes" << '\n' << std::endl;
 		}
 
 		// long int:
@@ -62,7 +63,7 @@ int main()
 		// Size of at least 32 bits. On LLP64 it will be 32 bits but other data models it will be 64 bits (LP64)
 		{
 			long int my_int = 2147483647; // min: -2147483648, max: 2147483647
-			std::cout << "Value of: " << my_int << ", Size: " << sizeof(my_int) << " bytes" << std::endl;
+			std::cout << "Value of: " << my_int << ", Size: " << sizeof(my_int) << " bytes" << '\n' << std::endl;
 		}
 
 		// long long int:
@@ -70,7 +71,7 @@ int main()
 		// Size of at least 64 bits. Added in C++11
 		{
 			long long int my_int = 9223372036854775807; // min: -92233720366854775808, max: 9223372036854775807
-			std::cout << "Value of: " << my_int << ", Size: " << sizeof(my_int) << " bytes" << std::endl;
+			std::cout << "Value of: " << my_int << ", Size: " << sizeof(my_int) << " bytes" << '\n' << std::endl;
 	
 			// Note: Code below causes compile error as integer cannot be represented by any integer type
 			// unsigned long long int another_int = 18446744073709551616;
@@ -85,6 +86,70 @@ int main()
 	}
 
 	// Floats:
-	// 
+	// float  	-> single precision floating-point type IEE-754 binary32 format.
+	// double 	-> double precision floating-point type IEE-754 binary64 format.
+	// long double  -> extended precision floating-point type: most well known is x87 80-bit extended precision format
+	// 		   used by x86-64 implementations.
+	//
+	// min_subnormal: Describes the absolute smallest size that can be represented by the data type.
+	//		  Sacrifices precision by allowing leading 0s for the significand to stop variable
+	//		  truncating to zero.
+	//
+	//		  32-bit : +- 0x1p-149 std::numeric_limits<float>::denorm_min();  
+	//		  64-bit : +- 0x1p-1074 std::numeric_limits<float>::denorm_min();  
+	//		  80-bit : +- 0x1p-16445 std::numeric_limits<float>::denorm_min();  
+	//		  128-bit: +- 0x1p-16494 std::numeric_limits<float>::denorm_min(); 
+	//
+	// min_normal   : Describes the smallest size that can be represented by the data type without losing precision.
+	// 		  
+	// 		  32-bit  : +- 0x1p-126 std::numeric_limits<float>::min();
+	// 		  64-bit  : +- 0x1p-1022
+	// 		  80-bit  : +- 0x1p-16382
+	// 		  128-bit : +- 0x1p-16382
+	//
+	// max		: Largest value that can be represented by the data type.
+	//			
+	//		  32-bit  : +- 0x1.fffffep+127 std::numeric_limits<float>::max();
+	//		  64-bit  : +- 0x1.fffffffffffffep+1023 std::numeric_limits<double>::max();
+	//		  80-bit  : +- 0x1.fffffffffffffffep+16383 std::numeric_limits<long double>::max();
+	//		  124-bit : +- 0x1.ffffffffffffffffffffffffffffp+16383 
+	//
+	// single precision floating-point type. IEE-754 binary32 format
+	{
+		// float
+		{
+			float my_true_min_float = std::numeric_limits<float>::denorm_min(); // same as subnormal
+			float my_min_float = std::numeric_limits<float>::min();
+			float my_max_float = std::numeric_limits<float>::max();
+			std::cout << "Value of DENORM_MIN: " << my_true_min_float << '\n';
+			std::cout << "Value of MIN: " << my_min_float << '\n';
+			std::cout << "Value of MAX: " << my_max_float << '\n';
+			std::cout << "Size: " << sizeof(my_max_float) << " bytes" << '\n';
+			std::cout << std::endl;
+		}
 
+		// double
+		{
+			double my_true_min_double = std::numeric_limits<double>::denorm_min();
+			double my_min_double = std::numeric_limits<double>::min();
+			double my_max_double = std::numeric_limits<double>::max();
+			std::cout << "Value of DENORM_MIN: " << my_true_min_double << '\n';
+			std::cout << "Value of MIN: " << my_min_double << '\n';
+			std::cout << "Value of MAX: " << my_max_double  << '\n';
+			std::cout << "Size: " << sizeof(my_max_double) << " bytes" << '\n';
+			std::cout << std::endl;
+		}
+		// long double
+		{
+			long double my_true_min_long_double = std::numeric_limits<long double>::denorm_min();
+			long double my_min_long_double = std::numeric_limits<long double>::min();
+			long double my_max_long_double = std::numeric_limits<long double>::max();
+			std::cout << "Value of DENORM_MIN: " << my_true_min_long_double << '\n';
+			std::cout << "Value of MIN: " << my_min_long_double << '\n';
+			std::cout << "Value of MAX: " << my_max_long_double  << '\n';
+			std::cout << "Size: " << sizeof(my_max_long_double) << " bytes" << '\n'; 
+			std::cout << std::endl;
+		}
+	}
+	
 }
